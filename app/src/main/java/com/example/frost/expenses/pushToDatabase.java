b1999a0bb25220146ea1767fd0c7cecd4f9debf1
Cindy <ong.wenling.cindy@dhs.sg>
2017-12-13 02:06:54 +0800

    print receipt in json

---
+++ app/src/main/java/com/example/frost/expenses/pushToDatabase.java
@@ -0,0 +1,13 @@
+package com.example.frost.expenses;
+
+/**
+ * Created by cindy on 13/12/2017.
+ */
+
+public class pushToDatabase {
+
+    public void addToDatabase(String jsonObject)
+    {
+
+    }
+}
